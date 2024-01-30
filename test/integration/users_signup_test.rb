16104e04a3cef271527f21774e58640b6f36a995
yamatowani <94161075+yamatowani@users.noreply.github.com>
2024-01-30 09:32:33 +0000

    Use SSL and Puma in pro

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,14 +17,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "valid signup information" do
-    assert_differense 'User.count', 1 do
+    assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
-      email: "user@example.com",
-      password:              "password",
-      password_confirmation: "password" } }
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
+    assert_not flash.blank?
   end
 end
-
